d4a87202ffe20d4d2e734a3f51c20146ea6bb7d6
Skyler Dowdy <skyler2440@gmail.com>
2019-04-02 19:00:16 -0400

    array-methods/callbacks missing last?

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,13 +2,13 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
-  //Given this problem: 
-  
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+
+  //Given this problem:
+
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
 
   // Potential Solution:
 
@@ -17,39 +17,61 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     return cb(arr[0]);
   }
 
-  // Function invocation 
+  // Function invocation
   firstItem(items, function(first) {
     console.log(first)
   });
 
-*/
 
 
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-}
+
+    function getLength(arr, cb) {
+        // console.log(items.length);
+        return cb(arr.length);
+    }
+    getLength(items, function(length) {
+    console.log(length);
+    });
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+    return cb(arr.splice(-1));
 }
+last(items, function (splice) {
+    console.log(splice);
+});
+
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+    // sumNums adds two numbers (x, y) and passes the result to the callback.
+    return cb(x, y);
 }
+const add = function (x, y) {
+    return x + y;
+};
+console.log(sumNums(2,4,add));
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+    return cb(x, y);
 }
+const mult = function (x, y) {
+    return x * y;
+};
+console.log(multiplyNums(2,4,mult));
+
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+    return cb(list.includes(item));
 }
+contains(item.items) === 'Pencil'
 
-/* STRETCH PROBLEM */
-
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+console.log(items.includes(`Pencil`));
+// /* STRETCH PROBLEM */
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+// }
